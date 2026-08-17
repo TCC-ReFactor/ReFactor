4566bd57cb7ec60ce583aa2f59c781f0cde11486
Nando <nandomathiasluiz@gmail.com>
2026-08-17 16:14:19 -0300

    Alteração modo de renderização específica para cada página

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'index',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'cadastro',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
