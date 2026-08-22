0bc53941b75fd5056bea1210060a0894346ffa48
Gusta656 <biellopes538@gmail.com>
2026-08-21 22:48:51 -0300

    alteração em nome das imagens e início de implementação de imagens otimizadas

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -1,8 +1,9 @@
 import { Component } from "@angular/core";
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: "app-cadastro",
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: "./cadastro.html",
   styleUrl: "./cadastro.css",
 })
--- src/app/pages/index/index.ts
+++ src/app/pages/index/index.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Router } from 'express';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-index',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './index.html',
   styleUrl: './index.css',
 })
 export class Index {
-
+  
 }
